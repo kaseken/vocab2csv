6d5068cab2af6cf3c012231bc978cbe95a646da9
kaseken <kent.kaseda.dev@gmail.com>
2025-08-25 00:53:07 +0900

    Enable app on Android

--- modules/vocab2csv-processor/android/src/main/java/expo/modules/vocab2csvprocessor/Vocab2CSVProcessorModule.kt
+++ modules/vocab2csv-processor/android/src/main/java/expo/modules/vocab2csvprocessor/Vocab2CSVProcessorModule.kt
@@ -1,17 +1,76 @@
 package expo.modules.vocab2csvprocessor
 
+import android.graphics.BitmapFactory
+import android.net.Uri
+import com.google.mlkit.vision.common.InputImage
+import com.google.mlkit.vision.text.TextRecognition
+import com.google.mlkit.vision.text.japanese.JapaneseTextRecognizerOptions
+import expo.modules.kotlin.Promise
 import expo.modules.kotlin.modules.Module
 import expo.modules.kotlin.modules.ModuleDefinition
+import java.io.File
 
 class Vocab2CSVProcessorModule : Module() {
-  // Each module class must implement the definition function. The definition consists of components
-  // that describes the module's functionality and behavior.
-  // See https://docs.expo.dev/modules/module-api for more details about available components.
   override fun definition() = ModuleDefinition {
-    // Sets the name of the module that JavaScript code will use to refer to the module. Takes a string as an argument.
-    // Can be inferred from module's class name, but it's recommended to set it explicitly for clarity.
-    // The module will be accessible from `requireNativeModule('Vocab2CSVProcessor')` in JavaScript.
     Name("Vocab2CSVProcessor")
 
+    AsyncFunction("processPhoto") { photoUri: String, promise: Promise ->
+      processPhoto(photoUri, promise)
+    }
+  }
+
+  private fun processPhoto(photoUri: String, promise: Promise) {
+    // Parse URI and create file
+    val uri: Uri
+    val file: File
+    try {
+      uri = Uri.parse(photoUri)
+      file = File(uri.path ?: "")
+    } catch (e: Exception) {
+      promise.reject("INVALID_URI", "Could not parse photo URI", e)
+      return
+    }
+
+    if (!file.exists()) {
+      promise.reject("INVALID_IMAGE", "Could not load image from URI", null)
+      return
+    }
+
+    val bitmap = try {
+      BitmapFactory.decodeFile(file.absolutePath)
+    } catch (e: Exception) {
+      promise.reject("DECODE_ERROR", "Could not decode image file", e)
+      return
+    }
+
+    if (bitmap == null) {
+      promise.reject("INVALID_IMAGE", "Could not decode image", null)
+      return
+    }
+
+    val image = try {
+      InputImage.fromBitmap(bitmap, 0)
+    } catch (e: Exception) {
+      promise.reject("IMAGE_PROCESSING_ERROR", "Could not create input image", e)
+      return
+    }
+
+    val recognizer = TextRecognition.getClient(JapaneseTextRecognizerOptions.Builder().build())
+
+    recognizer.process(image)
+      .addOnSuccessListener { visionText ->
+        val extractedText = mutableListOf<String>()
+        
+        for (block in visionText.textBlocks) {
+          for (line in block.lines) {
+            extractedText.add(line.text)
+          }
+        }
+
+        promise.resolve(extractedText)
+      }
+      .addOnFailureListener { e ->
+        promise.reject("VISION_ERROR", e.message ?: "Text recognition failed", e)
+      }
   }
 }
