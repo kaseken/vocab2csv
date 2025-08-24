d0fa404347fe4cfd057088f15e8c1e99170d0c85
kaseken <kent.kaseda.dev@gmail.com>
2025-08-24 16:10:06 +0900

    Delete unused components

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -15,7 +15,6 @@ export default function RootLayout() {
   return (
     <Stack>
       <Stack.Screen name="index" options={{ headerShown: false }} />
-      <Stack.Screen name="+not-found" />
     </Stack>
   );
 }
